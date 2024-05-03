b5f8265a1429f44eeb6c51aba0e7942a3c353d68
Tim Mitra <tim@it-guy.com>
2024-05-03 11:35:45 -0400

    Initial Commit

---
+++ Inspector/InspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectorApp.swift
+//  Inspector
+//
+//  Created by Tim Mitra on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct InspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
